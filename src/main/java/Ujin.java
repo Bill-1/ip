272ba248a0837abda54358aedfb0a9a983791baa
Bilguudei <bilguudei.shinezul23@gmail.com>
2025-01-23 16:41:53 +0800

    Level 1. Echo

--- src/main/java/Ujin.java
+++ src/main/java/Ujin.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 public class Ujin {
     public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
        String logo =
                         " _    _      _   _____   _   _  \n" +
                         "| |  | |    | | |_   _| | \\ | |\n" +
@@ -10,12 +12,20 @@ public class Ujin {
        System.out.println("Hello from\n" + logo);
        String botName = "Ujin";
        String horLine = "____________________________________________________________";
-       System.out.println(horLine);
-       System.out.println(" Hello! I'm " + botName);
-       System.out.println(" What can I do for you?\n");
-       System.out.println(horLine);
-       System.out.println("Bye. Hope to see you again soon!\n");
-       System.out.println(horLine);
+       System.out.println("\t" + horLine);
+       System.out.println("\t Hello! I'm " + botName);
+       System.out.println("\t What can I do for you?\n");
+       System.out.println("\t" + horLine);
+       String text;
+       do {
+          text = scanner.nextLine();
+          System.out.println("\t" + horLine);
+          System.out.println("\t " + text + '\n');
+          System.out.println("\t" + horLine + '\n');
+       } while(!(text.equals("Bye") || text.equals("bye") || text.equals("bYe") || text.equals("ByE") ||
+               text.equals("BYE") || text.equals("BYe") || text.equals("bYE") || text.equals("byE")));
+       System.out.println("\t Bye. Hope to see you again soon!\n");
+       System.out.println("\t" + horLine);
 
     }
 }
