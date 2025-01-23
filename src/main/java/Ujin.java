f1cf8ce647bb04c9ee5bfd5f9050f0a446b99672
Bilguudei <bilguudei.shinezul23@gmail.com>
2025-01-23 15:39:03 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Ujin.java
@@ -0,0 +1,21 @@
+public class Ujin {
+    public static void main(String[] args) {
+       String logo =
+                        " _    _      _   _____   _   _  \n" +
+                        "| |  | |    | | |_   _| | \\ | |\n" +
+                        "| |  | |    | |   | |   |  \\| |\n" +
+                        "| |  | | _  | |   | |   | |\\  |\n" +
+                        "| |  | || |_| |  _| |_  | | \\ |\n" +
+                        " \\____/  \\___/  |_____| |_| |_|\n";
+       System.out.println("Hello from\n" + logo);
+       String botName = "Ujin";
+       String horLine = "____________________________________________________________";
+       System.out.println(horLine);
+       System.out.println(" Hello! I'm " + botName);
+       System.out.println(" What can I do for you?\n");
+       System.out.println(horLine);
+       System.out.println("Bye. Hope to see you again soon!\n");
+       System.out.println(horLine);
+
+    }
+}
