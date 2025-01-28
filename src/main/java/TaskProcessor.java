ba6c17167c1ffec079fe3949679afc4602196595
Bilguudei <bilguudei.shinezul23@gmail.com>
2025-01-28 22:52:09 +0800

    Level 7. Save

---
+++ src/main/java/TaskProcessor.java
@@ -0,0 +1,121 @@
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+public class TaskProcessor {
+    protected static String folderPath = "./folder";
+    protected static String filePath = "./folder/tasks.txt";
+
+    public static void writeTasksToFile(List<Task> li) {
+        boolean firstTime = true;
+        for (Task task : li) {
+            String line = taskToText(task);
+            try (FileWriter writer = new FileWriter(filePath, !firstTime)) { // false means overwrite mode
+                writer.write(line + "\n"); // Writing a single line
+                firstTime = false;
+            } catch (IOException e) {
+                System.out.println("Error writing to file: " + e.getMessage());
+            }
+        }
+    }
+
+    public static List<Task> readTasksFromFile() {
+        List<Task> li = new ArrayList<>();
+
+        File folder = new File(folderPath);
+        if (folder.exists() && folder.isDirectory()) {
+            File file = new File(filePath);
+            if (file.exists() && file.isFile()) {
+                try{
+                    List<String> lines = Files.readAllLines(Paths.get(filePath));
+                    for (String line : lines) {
+                        li.add(textToTask(line));
+                    }
+                    return li;
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+            else {
+                try {
+                    Files.createFile(Paths.get(filePath));
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+        else {
+            Path path = Paths.get(folderPath);
+            try {
+                Files.createDirectory(path);
+                Files.createFile(Paths.get(filePath));
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return li;
+    }
+
+    public static String taskToText(Task task) {
+        String text = "";
+        if (task instanceof Todo) {
+            text += "T";
+        }
+        if (task instanceof Deadline) {
+            text += "D";
+        }
+        if (task instanceof Event) {
+            text += "E";
+        }
+        text += " | ";
+        text += (task.isDone ? "1" : "0");
+        text += " | ";
+        if (task instanceof Todo) {
+            text += task.description;
+        }
+        if (task instanceof Deadline) {
+            text += task.description;
+            text += " | ";
+            text += ((Deadline) task).by;
+        }
+        if (task instanceof Event) {
+            text += task.description;
+            text += " | ";
+            text += ((Event) task).start;
+            text += " | ";
+            text += ((Event) task).end;
+        }
+        return text;
+    }
+
+    public static Task textToTask(String text) {
+        Task task;
+        String[] parts = text.split("\\|");
+        for (int i = 0; i < parts.length; i++) {
+            parts[i] = parts[i].trim();
+        }
+        switch (parts[0]) {
+            case "T":
+                task = new Todo(parts[2]);
+                if (parts[0].equals("1")) {
+                    task.markAsDone();
+                }
+                break;
+            case "D":
+                task = new Deadline(parts[2], parts[3]);
+                if (parts[0].equals("1")) {
+                    task.markAsDone();
+                }
+                break;
+            case "E":
+                task = new Event(parts[2], parts[3], parts[4]);
+                if (parts[0].equals("1")) {
+                    task.markAsDone();
+                }
+                break;
+            default:
+                task = null;
+        }
+        return task;
+    }
+}
--- src/main/java/Ujin.java
+++ src/main/java/Ujin.java
@@ -1,158 +1,164 @@
 import java.sql.Array;
 import java.util.*;
+import java.io.*;
+
 public class Ujin {
     public static void main(String[] args) {
-          Scanner scanner = new Scanner(System.in);
-          String logo =
-                  " _    _      _   _____   _   _  \n" +
-                          "| |  | |    | | |_   _| | \\ | |\n" +
-                          "| |  | |    | |   | |   |  \\| |\n" +
-                          "| |  | | _  | |   | |   | |\\  |\n" +
-                          "| |  | || |_| |  _| |_  | | \\ |\n" +
-                          " \\____/  \\___/  |_____| |_| |_|\n";
-          System.out.println("Hello from\n" + logo);
-          String botName = "Ujin";
-          String horLine = "____________________________________________________________";
-          System.out.println("\t" + horLine);
-          System.out.println("\t Hello! I'm " + botName);
-          System.out.println("\t What can I do for you?\n");
-          System.out.println("\t" + horLine);
-          String text;
-          List<Task> li = new ArrayList<>();
-          do {
-             text = scanner.nextLine();
-             System.out.println("\t" + horLine);
-             try{
+
+        List<Task> li = TaskProcessor.readTasksFromFile();
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            TaskProcessor.writeTasksToFile(li);
+        }));
+        Scanner scanner = new Scanner(System.in);
+        String logo =
+                " _    _      _   _____   _   _  \n" +
+                        "| |  | |    | | |_   _| | \\ | |\n" +
+                        "| |  | |    | |   | |   |  \\| |\n" +
+                        "| |  | | _  | |   | |   | |\\  |\n" +
+                        "| |  | || |_| |  _| |_  | | \\ |\n" +
+                        " \\____/  \\___/  |_____| |_| |_|\n";
+        System.out.println("Hello from\n" + logo);
+        String botName = "Ujin";
+        String horLine = "____________________________________________________________";
+        System.out.println("\t" + horLine);
+        System.out.println("\t Hello! I'm " + botName);
+        System.out.println("\t What can I do for you?\n");
+        System.out.println("\t" + horLine);
+        String text;
+
+        do {
+            text = scanner.nextLine();
+            System.out.println("\t" + horLine);
+            try {
                 if (text.equals("list")) {
-                   for (int i = 0; i < li.size(); i++) {
-                      String index = String.valueOf(i + 1);
-                      System.out.println("\t " + index + "." + li.get(i));
-                   }
+                    for (int i = 0; i < li.size(); i++) {
+                        String index = String.valueOf(i + 1);
+                        System.out.println("\t " + index + "." + li.get(i));
+                    }
                 } else {
-                   String[] arr = text.split(" ");
-                   if (arr[0].equals("delete")){
-                      if (arr.length != 2) {
-                         throw new UjinException("Oops, it seems you put more or less variables :(");
-                      }
-                      int index;
-                      try {
-                         index = Integer.parseInt(arr[1]);
-                         Task deletedTask = li.get(index - 1);
-                         li.remove(index - 1);
-                         String size = String.valueOf(li.size());
-                         System.out.println("\t Noted. I've removed this task:\n");
-                         System.out.println("\t\t" + deletedTask + '\n');
-                         System.out.println("\t\tNow you have " + size + " tasks in the list.\n");
-                      }
-                      catch (Exception e) {
-                         System.out.println("\t Error!");
-                      }
-                   }
-                   else{
-                      if (arr[0].equals("mark")) {
-                         if (arr.length != 2) {
+                    String[] arr = text.split(" ");
+                    if (arr[0].equals("delete")) {
+                        if (arr.length != 2) {
                             throw new UjinException("Oops, it seems you put more or less variables :(");
-                         }
-                         int index;
-                         try {
+                        }
+                        int index;
+                        try {
                             index = Integer.parseInt(arr[1]);
-                            li.get(index - 1).markAsDone();
-                            System.out.println("\t Nice! I've marked this task as done:\n");
-                            System.out.println("\t\t" + li.get(index - 1));
-                         } catch (Exception e) {
+                            Task deletedTask = li.get(index - 1);
+                            li.remove(index - 1);
+                            String size = String.valueOf(li.size());
+                            System.out.println("\t Noted. I've removed this task:\n");
+                            System.out.println("\t\t" + deletedTask + '\n');
+                            System.out.println("\t\tNow you have " + size + " tasks in the list.\n");
+                        } catch (Exception e) {
                             System.out.println("\t Error!");
-                         }
-                      } else {
-                         if (arr[0].equals("unmark")) {
+                        }
+                    } else {
+                        if (arr[0].equals("mark")) {
                             if (arr.length != 2) {
-                               throw new UjinException("Oops, it seems you put more or less variables :(");
+                                throw new UjinException("Oops, it seems you put more or less variables :(");
                             }
                             int index;
                             try {
-                               index = Integer.parseInt(arr[1]);
-                               li.get(index - 1).unmarkAsDone();
-                               System.out.println("\t OK, I've marked this task as not done yet:\n");
-                               System.out.println("\t\t" + li.get(index - 1));
+                                index = Integer.parseInt(arr[1]);
+                                li.get(index - 1).markAsDone();
+                                System.out.println("\t Nice! I've marked this task as done:\n");
+                                System.out.println("\t\t" + li.get(index - 1));
                             } catch (Exception e) {
-                               System.out.println("Oops, you should put a number!");
+                                System.out.println("\t Error!");
                             }
-                         } else {
-                            boolean flag = true, secondFlag = false;
-                            String start = "", end = "", task = "";
-                            for (int i = 0; i < arr.length; i++) {
-                               String currentText = arr[i];
-                               if (currentText.charAt(0) == '/') {
-                                  if (flag) {
-                                     flag = false;
-                                  } else {
-                                     secondFlag = true;
-                                  }
-                               } else {
-                                  if (flag && i != 0) {
-                                     task += (currentText + " ");
-                                  }
-                                  if (!flag && !secondFlag) {
-                                     start += (" " + currentText);
-                                  }
-                                  if (secondFlag) {
-                                     end += (" " + currentText);
-                                  }
-                               }
-                            }
-                            Task newTask;
-                            String size;
-                            switch (arr[0]) {
-                               case "todo":
-                                  if (arr.length == 1) {
-                                     throw new UjinException("Oops! There is no task!");
-                                  }
-                                  newTask = new Todo(task);
-                                  li.add(newTask);
-                                  System.out.println("\t " + "Got it. I've added this task:\n" + "\t\t" + newTask + '\n');
-                                  size = String.valueOf(li.size());
-                                  System.out.println("\t " + "Now you have " + size + " tasks in the list.\n");
-                                  break;
-                               case "deadline":
-                                  if (arr.length == 1) {
-                                     throw new UjinException("Oops! There is no task!");
-                                  }
-                                  newTask = new Deadline(task, start);
-                                  li.add(newTask);
-                                  System.out.println("\t " + "Got it. I've added this task:\n" + "\t\t" + newTask + '\n');
-                                  size = String.valueOf(li.size());
-                                  System.out.println("\t " + "Now you have " + size + " tasks in the list.\n");
-                                  break;
-                               case "event":
-                                  if (arr.length == 1) {
-                                     throw new UjinException("Oops! There is no task!");
-                                  }
-                                  newTask = new Event(task, start, end);
-                                  li.add(newTask);
-                                  System.out.println("\t " + "Got it. I've added this task:\n" + "\t\t" + newTask + '\n');
-                                  size = String.valueOf(li.size());
-                                  System.out.println("\t " + "Now you have " + size + " tasks in the list.\n");
-                                  break;
-                               case "bye":
-                                  break;
-                               default:
-                                  throw new UjinException("Please check the first word! It should be about the task!");
-                            }
+                        } else {
+                            if (arr[0].equals("unmark")) {
+                                if (arr.length != 2) {
+                                    throw new UjinException("Oops, it seems you put more or less variables :(");
+                                }
+                                int index;
+                                try {
+                                    index = Integer.parseInt(arr[1]);
+                                    li.get(index - 1).unmarkAsDone();
+                                    System.out.println("\t OK, I've marked this task as not done yet:\n");
+                                    System.out.println("\t\t" + li.get(index - 1));
+                                } catch (Exception e) {
+                                    System.out.println("Oops, you should put a number!");
+                                }
+                            } else {
+                                boolean flag = true, secondFlag = false;
+                                String start = "", end = "", task = "";
+                                for (int i = 0; i < arr.length; i++) {
+                                    String currentText = arr[i];
+                                    if (currentText.charAt(0) == '/') {
+                                        if (flag) {
+                                            flag = false;
+                                        } else {
+                                            secondFlag = true;
+                                        }
+                                    } else {
+                                        if (flag && i != 0) {
+                                            task += (currentText + " ");
+                                        }
+                                        if (!flag && !secondFlag) {
+                                            start += (" " + currentText);
+                                        }
+                                        if (secondFlag) {
+                                            end += (" " + currentText);
+                                        }
+                                    }
+                                }
+                                Task newTask;
+                                String size;
+                                switch (arr[0]) {
+                                    case "todo":
+                                        if (arr.length == 1) {
+                                            throw new UjinException("Oops! There is no task!");
+                                        }
+                                        newTask = new Todo(task);
+                                        li.add(newTask);
+                                        System.out.println("\t " + "Got it. I've added this task:\n" + "\t\t" + newTask + '\n');
+                                        size = String.valueOf(li.size());
+                                        System.out.println("\t " + "Now you have " + size + " tasks in the list.\n");
+                                        break;
+                                    case "deadline":
+                                        if (arr.length == 1) {
+                                            throw new UjinException("Oops! There is no task!");
+                                        }
+                                        newTask = new Deadline(task, start);
+                                        li.add(newTask);
+                                        System.out.println("\t " + "Got it. I've added this task:\n" + "\t\t" + newTask + '\n');
+                                        size = String.valueOf(li.size());
+                                        System.out.println("\t " + "Now you have " + size + " tasks in the list.\n");
+                                        break;
+                                    case "event":
+                                        if (arr.length == 1) {
+                                            throw new UjinException("Oops! There is no task!");
+                                        }
+                                        newTask = new Event(task, start, end);
+                                        li.add(newTask);
+                                        System.out.println("\t " + "Got it. I've added this task:\n" + "\t\t" + newTask + '\n');
+                                        size = String.valueOf(li.size());
+                                        System.out.println("\t " + "Now you have " + size + " tasks in the list.\n");
+                                        break;
+                                    case "bye":
+                                        break;
+                                    default:
+                                        throw new UjinException("Please check the first word! It should be about the task!");
+                                }
 
-                         }
-                      }
-                   }
+                            }
+                        }
+                    }
                 }
-             }
-             catch (UjinException e) {
+            } catch (UjinException e) {
                 System.out.println("\t" + e.getMessage());
-             }
+            }
 
-             if (!text.equals("bye")) {
+            if (!text.equals("bye")) {
                 System.out.println("\t" + horLine + '\n');
-             }
-          } while (!(text.equals("Bye") || text.equals("bye") || text.equals("bYe") || text.equals("ByE") ||
-                  text.equals("BYE") || text.equals("BYe") || text.equals("bYE") || text.equals("byE")));
-          System.out.println("\t Bye. Hope to see you again soon!\n");
-          System.out.println("\t" + horLine);
+            }
+        } while (!(text.equals("Bye") || text.equals("bye") || text.equals("bYe") || text.equals("ByE") ||
+                text.equals("BYE") || text.equals("BYe") || text.equals("bYE") || text.equals("byE")));
+        System.out.println("\t Bye. Hope to see you again soon!\n");
+        System.out.println("\t" + horLine);
+        TaskProcessor.writeTasksToFile(li);
     }
+
 }
